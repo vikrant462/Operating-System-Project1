1e85c65ccdcbde1dd44275fbe258141a3c8a342e
Vikrant Chaudhary <cvikrant462@gmail.com>
2019-03-22 19:17:01 +0530

    Update OS1.c
    Completion_Time,Turn_Around_Time And Waiting_Time Included.

--- OS1.c
+++ OS1.c
@@ -1,26 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include<unistd.h>
+/*
+Arrival Time:       Time at which the process arrives in the ready queue.
+Completion Time:    Time at which process completes its execution.
+Burst Time:         Time required by a process for CPU execution.
+Turn Around Time:   Time Difference between completion time and arrival time.          
+     Turn Around Time = Completion Time - Arrival Time
 
+Waiting Time(W.T): Time Difference between turn around time and burst time.
+     Waiting Time = Turn Around Time - Burst Time
+*/
 struct Process
 {
-    int arrival_time,burst_time,waiting_time,turnaround_time,flag;
-};
+    int P_ID,arrival_time,burst_time,completion_time,waiting_time,turnaround_time,flag;
+}Process;
 
 int main()
 {
 	int n,i,j,burst_count=0;
 	printf("Enter The number of Process to enter\n");
 	scanf("%d",&n);
-	//n=8;
+	
     struct Process P[n];
     for(i=0;i<n;i++)
     {
+    	P[i].P_ID=i+1;
     	printf("Enter arrival time for %d process\n",i+1);
 	    scanf("%d",&P[i].arrival_time);
 	    
-	    printf("Enter burst time for %d process\n",i+1);
-	    scanf("%d",&P[i].burst_time);
+	    //printf("Enter burst time for %d process\n",i+1);
+	    //scanf("%d",&P[i].burst_time);
+	    P[i].burst_time=2*P[i].arrival_time;
 	    burst_count+=P[i].burst_time;
 	}
 	
@@ -30,8 +41,7 @@ int main()
 		for(i=0;i<n-j-1;i++)
 		{
 			if(P[i].arrival_time>P[i+1].arrival_time)
-			{
-			printf("check");	
+			{	
 			struct Process t=P[i];
 			P[i]=P[i+1];
 			P[i+1]=t;
@@ -39,41 +49,36 @@ int main()
 	    }
 	}
 	/////////////////////////////////////////////////
-	for(i=0;i<n;i++)
+   /*	for(i=0;i<n;i++)
     {
 	    printf("%d  :  %d\n",P[i].arrival_time,P[i].burst_time);
-	}
+	}*/
 	///////////////////////////////////////////////////
 	int time_t=P[0].arrival_time,count_n=0;
     do
 	{
-		int max=0,temp_i,temp_change=0;
+		int max=0,temp_i;
 		for(i=0;P[i].arrival_time<=time_t;i++)
 		{
 			//finding the biggest burst time at time time_t
-	        //if(P[i].burst_time<=time_t && P[i].flag!=555)
-	        //{
+	        //if P[i].flag=555 then it means this process is already done
 	        	if(P[i].burst_time>=max && P[i].flag!=555)
 	        	{
-	        		//printf("Hello");
 	        	max=P[i].burst_time;
 	        	temp_i=i;
-	        	temp_change=1;
-	        	//count_n++;
 	            }
-	            //printf("world");
-			//}
-			//time_t=max;
         }
         
-        if(temp_change==1)
-		{
-		printf("%d :  %d ======",P[temp_i].arrival_time,P[temp_i].burst_time);
-        printf("%d : %d\n",burst_count,max);
+		
         P[temp_i].flag=555;
+        P[i].completion_time=time_t+max;
+        P[i].turnaround_time=P[i].completion_time-P[i].arrival_time;
+        P[i].waiting_time=P[i].turnaround_time-P[i].burst_time;
+        
+        printf("%d :  %d  :   %d======",P[temp_i].arrival_time,P[temp_i].burst_time,P[i].completion_time);
+        printf("%d : %d\n",burst_count,max);
         time_t+=max;
         burst_count-=max;
-        }
         
 	}while(burst_count>0);
 }
